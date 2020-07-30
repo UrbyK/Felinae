e062a03a2270f330c70b725c9c8ddc575b54d840
UrbyX9 <klinc.urban97@gmail.com>
2020-07-30 20:27:27 +0200

    Felina start

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" content="text/html">
+        <meta name="viewport" content="width=device-width, intital-scale=1.0">
+
+        <!-- Custom CSS -->
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+
+        <title>Felinae</title>
+    </head>
+    
+    <body>
+        <header>
+
+        </header>
+
+    <div class="container-fluid">
