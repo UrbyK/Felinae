b1a5a7e4d637c6ed4602a82259e0e52400a78066
UrbyX9 <klinc.urban97@gmail.com>
2020-08-10 12:20:56 +0200

    Registration

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
     include './includes/dbh.inc.php';
 
+    include_once('./includes/search-functions.php');
+
     function template_header($title){
         include_once('./header.php');
     }
@@ -8,5 +10,15 @@
     function template_footer(){
         include_once('./footer.php');
     }
+
+
+    function countries(){
+        $pdo = pdo_connect_mysql();
+        $stmt = $pdo->prepare("SELECT * FROM country");
+        $stmt->execute();
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+    }
+
 
 ?>
