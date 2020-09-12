6055d7f11dcb10e8fc1efdcf682082a50693ab4f
UrbyX9 <klinc.urban97@gmail.com>
2020-09-12 22:53:22 +0200

    old files for backup

---
+++ old-files/includes/search-functions.php
@@ -0,0 +1,9 @@
+<?php
+    function get_city($pdo, $city, $postalCode, $country_id){
+
+        $query = "SELECT * FROM city WHERE country_id = ? AND lower(city) LIKE lower(?) AND postalCode = ?";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$country_id, $city, $postalCode]);
+        return $stmt;
+    }
+?>
