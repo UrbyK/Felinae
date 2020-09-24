15e42db07a5a9b6df98b6fdf2014efdaa222fa46
UrbyX9 <klinc.urban97@gmail.com>
2020-09-24 14:07:15 +0200

    register added required fields

--- register.php
+++ register.php
@@ -19,7 +19,7 @@
                                 <div class="form-group row">
                                     <label class="col-md-4 col-form-label text-md-right" for="username">Uporabniško ime<span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" class="form-control" name="username" id="username" placeholder="Uporabniško ime" pattern=".{4,}"  limit="60">
+                                        <input type="text" class="form-control" name="username" id="username" placeholder="Uporabniško ime" pattern=".{4,}" limit="60" required>
                                         <span class="hint">Ime mora biti dolgo vsaj 4 znake.</span>
                                        
                                     </div><!-- col-md-6 -->
@@ -27,13 +27,13 @@
                                 <div class="form-group row">
                                     <label class="col-md-4 col-form-label text-md-right" for="email">Elektronska pošta<span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="email" class="form-control" name="email" id="email" placeholder="Elektronska pošta"  limit="150">
+                                        <input type="email" class="form-control" name="email" id="email" placeholder="Elektronska pošta" limit="150" required>
                                     </div><!-- col-md-6 -->
                                 </div><!-- form-group row -->
                                 <div class="form-group row">
                                     <label class="col-md-4 col-form-label text-md-right" for="password">Geslo<span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,}" class="form-control" name="password" id="password" placeholder="Geslo" data-limit=255  autocomplete="off">
+                                        <input type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9]).{8,}" class="form-control" name="password" id="password" placeholder="Geslo" data-limit=255  autocomplete="off" required>
                                         <span class="hint" id="minCharLenght">Geslo mora vsebovati najmanj 8 znakov!</span><br/>
                                         <span class="hint" id="bigChar">Vsebovati mora veliko črko [A-Ž],</span>
                                         <span class="hint" id="smallChar">malo črko [a-ž],</span>
@@ -46,7 +46,7 @@
                                 <div class="form-group row">
                                     <label class="col-md-4 col-form-label text-md-right" for="confirm_password">Potrdite geslo<span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Potrdite geslo"  autocomplete="off">
+                                        <input type="password" class="form-control" name="confirm_password" id="confirm_password" placeholder="Potrdite geslo" autocomplete="off" required>
                                         <span id="message"></span>
                                     </div><!-- col-md-6 -->
                                 </div><!-- form-group row -->
@@ -58,13 +58,13 @@
                                 <div class="form-group row">
                                     <label class="col-md-4 col-form-label text-md-right" for="fname">Ime<span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" class="form-control" name="fname" id="fname" placeholder="Ime"  limit="90">
+                                        <input type="text" class="form-control" name="fname" id="fname" placeholder="Ime" limit="90" required>
                                     </div><!-- col-md-6 -->
                                 </div><!-- form-group row -->
                                 <div class="form-group row">
                                     <label class="col-md-4 col-form-label text-md-right" for="lname">Priimek<span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <input type="text" class="form-control" name="lname" id="lname" placeholder="Priimek"  limit="90">
+                                        <input type="text" class="form-control" name="lname" id="lname" placeholder="Priimek" limit="90" required>
                                     </div><!-- col-md-6 -->
                                 </div><!-- form-group row -->
                                 <div class="form-group row">
@@ -77,26 +77,27 @@
                                 <div class="form-group row">
                                     <lable class="col-md-4 col-form-label text-md-right" for="address">Naslov<span class="required">*</span></lable>
                                     <div class="col-md-6">
-                                        <input type="text" class="form-control" name="address" id="address" placeholder="Velika ulica, 4"  limit="100">
+                                        <input type="text" class="form-control" name="address" id="address" placeholder="Velika ulica, 4" limit="100" required>
                                     </div><!-- col-md-6 -->
                                 </div><!-- form-group row -->
                                 <div class="form-group row">
                                     <lable class="col-md-4 col-form-label text-md-right" for="postalCode">Poštna številka<span class="required">*</span></lable>
                                     <div class="col-md-6">
-                                        <input type="text" class="form-control" name="postalCode" id="postalCode" placeholder="1000"  limit="13">
+                                        <input type="text" class="form-control" name="postalCode" id="postalCode" placeholder="1000" limit="13" required>
                                     </div><!-- col-md-6 -->
                                 </div><!-- form-group row -->
                                 <div class="form-group row">
                                     <lable class="col-md-4 col-form-label text-md-right" for="city">Mesto<span class="required">*</span></lable>
                                     <div class="col-md-6">
-                                        <input type="text" class="form-control" name="city" id="city" placeholder="Ljubljana"  limit="60">
+                                        <input type="text" class="form-control" name="city" id="city" placeholder="Ljubljana" limit="60" required>
                                     </div><!-- col-md-6 -->
                                 </div><!-- form-group row -->
 
                                 <div class="form-group row">
                                     <label class="col-md-4 col-form-label text-md-right" for="country">Država<span class="required">*</span></label>
                                     <div class="col-md-6">
-                                        <select name="country" id="country" class="form-control">
+                                        <select name="country" id="country" class="form-control" required>
+                                        <option hidden disabled selected value>---N/A---</option>
                                         <?php foreach (countries() as $item): ?>
                                             <option value="<?=$item['id']?>"><?=$item['country']?></option>
                                         <?php endforeach; ?>
