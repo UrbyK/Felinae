8a10fc75493fd4167f6d48fd96cb901460466a14
UrbyX9 <klinc.urban97@gmail.com>
2020-10-01 19:06:37 +0200

    Multiple category and parent insert

--- category-add.php
+++ category-add.php
@@ -11,26 +11,34 @@
                     </div><!--card-header -->
                     <div class="card-body">
                         <form method="post" class="content" action="./index.php?page=src/inc/category-insert.inc" enctype="multipart/form-data">
-                            <div class="form-group">
-                                <label class="col-md-4 col-form-label text-md-left" for="category">Kategorija</label>
-                                <div class="col">
-                                    <input type="text" class="form-control" name="category" id="category" required>
-                                    <option></option>
-                                </div><!-- col --> 
-                                <label class="col-md-4 col-form-label text-md-left" for="parent-catagory">Nad kategorija</label>
-                                <div class="col">
-                                    <select name="parent_category" id="parent_category" class="form-control">
+                            
+
+                            <div class="form-row">
+                                <div class="form-group col-md-5">
+                                    <label for="category">Kategorija</label>
+                                    <input type="text" class="form-control" id="category" name="category[]">
+                                </div>
+
+                                <div class="form-group col-md-5">
+                                    <label for="parent_category">Nad kategorija</label>
+                                    <select name="parent_category[]" id="parent_category" class="form-control">
                                         <option selected value="">---N/A---</option>
                                         <?php foreach (all_catagory() as $item): ?>
                                             <option value="<?=$item['id']?>"><?=$item['category']?></option>
                                         <?php endforeach; ?>
                                     </select>
                                 </div>
-                            </div><!-- form-group -->
-                            <hr/>
-                            <div class="col-md-3 offset-md-2 float-right float-sm-right">
-                                <button type="submit" name="Submit" class="btn btn-primary" id="cat_ins">Dodaj</button>
-                            </div><!-- col-md-3 offset-md-2 float-right float-sm-right -->
+                                <div class="form-group col-md-1">
+                                    <button class="add_form_field btn btn-success"><i class="fa fa-plus"></i></button>
+                                </div>
+
+                            </div>
+
+                            <div class="form-row">
+                                <div class="col-md-3 offset-md-2 float-right float-sm-right">
+                                    <button type="submit" name="Submit" class="btn btn-primary" id="cat_ins">Shrani</button>
+                                </div><!-- col-md-3 offset-md-2 float-right float-sm-right -->
+                            </div>
 
                         </form>
                     </div><!-- card-body -->
@@ -39,3 +47,47 @@
         </div><!-- row justify-content-center -->
     </div><!-- my-form -->
 </div> <!--CONTAINER-->
+
+<script>
+    $(document).ready(function() {
+        var max_fields = 5;
+        var wrapper = $(".content");
+        var add_button = $(".add_form_field");
+
+        var x = 1;
+        $(add_button).click(function(e) {
+            e.preventDefault();
+            if(x < max_fields) {
+                x++;
+                $(wrapper).append(
+                    
+                    '<div class="form-row">\
+                    <div class="form-group col-md-5">\
+                        <label for="category">Kategorija</label>\
+                        <input type="text" class="form-control" id="category" name="category[]">\
+                    </div>\
+                    <div class="form-group col-md-5">\
+                        <label for="parent_category">Nad kategorija</label>\
+                        <select name="parent_category[]" id="parent_category" class="form-control">\
+                            <option selected value="">---N/A---</option>\
+                            <?php foreach (all_catagory() as $item): ?>\
+                                <option value="<?=$item['id']?>"><?=$item['category']?></option>\
+                            <?php endforeach; ?>\
+                        </select>\
+                    </div>\
+                    <div class="form-group col-md-1">\
+                        <button class="delete_form_field btn btn-danger"><i class="fa fa-minus"></i></button>\
+                    </div>'
+                );
+            } else {
+                alert('Dosegli ste limit za število dodajanih vrstic');
+            }
+        });
+
+        $(wrapper).on("click", ".delete_form_field", function(e) {
+        e.preventDefault();
+        $(this).closest('.form-row').remove();
+        x--;
+        });
+    });
+</script>
